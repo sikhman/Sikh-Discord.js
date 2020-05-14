7208415ea0a69dd90bbe0ed71e80942c9b5a4dc0
sikhman <65096152+sikhman@users.noreply.github.com>
2020-05-13 20:42:06 -0400

    first push

---
+++ index.js
@@ -0,0 +1,45 @@
+const Discord = require('discord.js');
+const { prefix, Token} = require('./config.json');
+const client = new Discord.Client();
+const { Client, MessageEmbed } = require('discord.js');
+
+
+client.once('ready', () => {
+    console.log('Ready!');
+    client.user.setActivity('Seva', {type: 'PLAYING'}).catch(console.error);
+})
+client.on('message', message => {
+
+
+    if (message.content === '!amrit') {
+
+
+        const embed = new MessageEmbed()
+        .setTitle('Amrit')
+        .setColor(0xff0000)
+        .setDescription('Amrit means nectar, it consists of water and sugar. It is stirred with a double edged sword (Khanda) well prayers are said during the Khanda-Ki-Pahul')
+        .setThumbnail('https://www.sikhiwiki.org/images/3/3e/AmritSanskar-AW.jpg')
+        message.channel.send(embed);
+    
+    
+}
+});
+
+client.on('message', message => {
+
+
+    if (message.content === '!kirpan') {
+
+
+        const embed = new MessageEmbed()
+        .setTitle('Kirpan')
+        .setColor(0xff0000)
+        .setDescription('The Kirpan is any sword or dagger of any size and shape, it is one of the Five Ks, of the Khalsa Sikhs. The word kirpan has the meaning of weapon of defence, as apposed to talwar a weapon of offense')
+        .setThumbnail('https://www.mrsikhnet.com/wp-content/uploads/2010/03/khalsakirpan.jpg')
+        message.channel.send(embed);
+    
+    
+}
+});
+
+client.login(Token);
